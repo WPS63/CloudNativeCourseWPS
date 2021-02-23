ae5b9816b37d1598d1101e35148a1d0d1035f174
Bill Shaughnessy <wshaughn@uncc.edu>
2021-02-22 21:34:55 -0500

    lab4

--- lab4/webserver.go
+++ lab4/webserver.go
@@ -1,30 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	db := database{"shoes": 50, "socks": 5}
+	db := database{"shoes": 50, "socks": 5} //create an instance of type database
 	http.HandleFunc("/list", db.list)
 	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-type dollars float32
+type dollars float32 //declare type dollars
 
-func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) } //only keep 2 decimal places of dollars
 
-type database map[string]dollars
+type database map[string]dollars //databases are maps of items and their dollar value
 
-func (db database) list(w http.ResponseWriter, req *http.Request) {
+func (db database) list(w http.ResponseWriter, req *http.Request) { //prints all items in the database
 	for item, price := range db {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
 }
-
 func (db database) price(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
 	if price, ok := db[item]; ok {
@@ -33,4 +39,17 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusNotFound) // 404
 		fmt.Fprintf(w, "no such item: %q\n", item)
 	}
+}
+func (db database) create(w http.ResponseWriter, req *http.Request) {
+	//add new item to
+}
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+
+}
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter item to delete: ")
+	text, _ := reader.ReadString('\n')
+
+	delete(db, text)
 }
